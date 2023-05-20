7ec31c82999e20c61fb64fad9f43fd3333f0f16b
Magomedcoder <info@magomedcoder.ru>
2023-05-20 15:16:54 +0300

    first commit

---
+++ ChatGPT/ChatGPTApp.swift
@@ -0,0 +1,15 @@
+//
+//  ChatGPTApp.swift
+//  ChatGPT
+//
+//  Created by Magomedcoder on 20.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct ChatGPTApp: App {
+    var body: some Scene {
+        WindowGroup {}
+    }
+}
